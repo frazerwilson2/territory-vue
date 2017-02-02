865c3fbbfac6b3142827674418a5cd6c7e408d5d
frazer wilson <frazerwilson145@gmail.com>
2017-02-02 23:50:29 +0000

    picking roster functionality

--- app.js
+++ app.js
@@ -15,31 +15,24 @@ var app = new Vue({
       arena:4,
       legends:[]
     },
-    player: {
-      name: '',
-      cash:50,
-      roster:[],
-      stories:[],
-      gimmicks:[],
-      goal:0,
-      tokens: 0
-    },
     currentPlayer:0
   },
   mounted: function() {
   this.getRoster();
   },
   methods: {
-    nextPlayer: function(){
-      this.currentPlayer + 1 == this.game.players.length ? this.currentPlayer = 0 : this.currentPlayer++;
-    },
-    fetchEvents: function(){
-      fetch('/data.json')
-      .then(blob => blob.json())
-      .then(data => this.$set(this,'todos', data));
-    },
     newPlayer: function(){
-      this.game.players.push(this.player);
+      var nextup = {
+        name: '',
+        cash:50,
+        roster:[],
+        stories:[],
+        gimmicks:[],
+        goal:0,
+        tokens: 0,
+        ready: false
+      };
+      this.game.players.push(nextup);
     },
     getRoster:function(){
       fetch('/roster.json')
@@ -48,18 +41,41 @@ var app = new Vue({
     },
     pickRoster: function(){
       this.game.roster = this.rawRoster.map(w => w.Id);
+    },
+    // The setup roster pick
+    addToRoster: function(data){
+      this.game.players[this.currentPlayer].roster.push(data.id);
+      this.game.players[this.currentPlayer].cash -= data.val;
+    },
+    nextPlayer: function(){
+      this.currentPlayer + 1 == this.game.players.length ? this.currentPlayer = 0 : this.currentPlayer++;
+      if(this.game.players[this.currentPlayer].ready){this.nextPlayer()}
+    },
+    donePurchase: function(){
+      this.game.players[this.currentPlayer].ready = true;
+      var readyCount = 0;
+      for(var i=0;i<this.game.players.length;i++){
+        if (this.game.players[i].ready) {readyCount++}
+      }
+      if(readyCount == this.game.players.length) {
+        alert('lets play');
+      }
+      else {
+      this.nextPlayer();  
+      }     
     }
   },
   components: {
     'wrestler': {
-      props: ['data'],
-      template: '<div class="box">\
-      <h2>{{data.Name}}</h2>\
-      ({{data.Val}}) <a v-on:click="increment()">Purchase</a>\
+      props: ['data', 'cash'],
+      template: '<div class="box" v-bind:class="{ assigned: data.assigned || !cash }">\
+      <h2>{{data.Name}} ({{data.val}})</h2>\
+      ({{data.Val}}) <a v-on:click="purchaseRoster()" v-if="!data.assigned">Purchase</a>\
       </div>',
       methods: {
-        increment: function () {
-          console.log(this.data.Name)
+        purchaseRoster: function () {
+          this.data.assigned = true;
+          app.$emit('addToRoster', {id:this.data.Id, val:this.data.val});
           app.$emit('nextPlayer', 'switch to' + this.data.Id);
         }
       }
@@ -67,8 +83,11 @@ var app = new Vue({
   }
 });
 
-app.$on('nextPlayer', function (msg) {
+app.$on('nextPlayer', function (x) {
   this.nextPlayer();
+})
+app.$on('addToRoster', function (data) {
+  this.addToRoster(data);
 })
 
 /*
